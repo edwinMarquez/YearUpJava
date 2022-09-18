5eebc02f922d119942fb5c8067abbfcc2626cf22
edwinMarquez <edwinmarquezh@gmail.com>
2022-09-17 23:34:58 -0700

    yearupSpring added webController and index page

---
+++ yearupSpring/src/main/java/com/example/yearupSpring/controllers/WebController.java
@@ -0,0 +1,18 @@
+package com.example.yearupSpring.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    String indexPage(Model model){
+        return "index";
+    }
+
+
+
+}
