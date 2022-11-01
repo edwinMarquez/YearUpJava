d1f6ef541a52b6adf61f33adeab48c253270e8a0
VidhyaRamamoorthy <vidhyarpsg@gmail.com>
2022-11-01 17:04:38 -0400

    Adding java basics exercises from class

---
+++ Lectures/week2/CompilationProgram.java
@@ -0,0 +1,6 @@
+class Main {   
+   public static void main(String args[]) {      
+         int t;      //This program wouldn't run as the value is not initialized.
+         System.out.println(t); 
+    }   
+}
