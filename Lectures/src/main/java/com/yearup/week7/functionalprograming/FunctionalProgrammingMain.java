2d33253c8f34a18ffbb2f09217e255e113cb9122
Edwin Marquez <edwinmarquezh@gmail.com>
2022-11-10 21:36:07 -0800

    Functional programming sample, changed program to avoid randoms as sample (better to be discussed instead)

--- Lectures/src/main/java/com/yearup/week7/functionalprograming/FunctionalProgrammingMain.java
+++ Lectures/src/main/java/com/yearup/week7/functionalprograming/FunctionalProgrammingMain.java
@@ -3,40 +3,26 @@ package com.yearup.week7.functionalprograming;
 import com.yearup.week7.functionalprograming.finterfaces.DoubleGenerator;
 import com.yearup.week7.functionalprograming.finterfaces.DoubleGenerator2Param;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.function.Function;
-
 public class FunctionalProgrammingMain {
 
     public static void main(String[] args){
 
         //lambda syntax.
-        DoubleGenerator randomNum = () -> Math.random(); //lambda expression.
-        System.out.println("Random Num 0-9: " + randomNum.getValue());
+        DoubleGenerator logBaseE = (param) -> Math.log(param); //lambda expression.
+        System.out.println("Log base e of 10 is: " + logBaseE.getValue(10L));
 
-        DoubleGenerator randomNum1to100 = () -> Math.random()*99d + 1d; //up to 99.999....
-        System.out.println("Random Num 1-100: " + randomNum1to100.getValue());
+        //using two parameters
+        DoubleGenerator2Param logBase10 = (base, value) -> Math.log(value)/Math.log(base);
+        System.out.println("Log base 10, of 100 is: " + logBase10.getValue(10d,100d));
 
         //this is how an anonymous class would look like instead:
-        DoubleGenerator anonymousRandomGen = new DoubleGenerator() {
+        DoubleGenerator anonymousLogBaseE = new DoubleGenerator() {
             @Override
-            public Double getValue() {
-                return Math.random();
+            public Double getValue(long param) {
+                return Math.log(param);
             }
         };
-
-        //we might as well use block with our lambda expressions. if we need to use multiple statement:
-        DoubleGenerator secureRandomGenerator = () -> {
-            SecureRandom secureRandom = new SecureRandom();
-            return secureRandom.nextDouble();
-        };
-
-        //with parameters.
-        DoubleGenerator2Param randomInRange = (rStart, rEnd) -> rStart + Math.random()*(rEnd-rStart);//will fail when rStart > rEnd
-        System.out.println("random from 10 to 20: " + randomInRange.getValue(10d,20d));
-
+        System.out.println("Log base e of 10 is: " + anonymousLogBaseE.getValue(10L));
 
     }
 
--- Lectures/src/main/java/com/yearup/week7/functionalprograming/finterfaces/DoubleGenerator2Param.java
+++ Lectures/src/main/java/com/yearup/week7/functionalprograming/finterfaces/DoubleGenerator2Param.java
@@ -2,5 +2,5 @@ package com.yearup.week7.functionalprograming.finterfaces;
 
 @FunctionalInterface
 public interface DoubleGenerator2Param {
-    Double getValue(Double rangeStart, Double rangeEnd);
+    Double getValue(Double param1, Double param2);
 }
